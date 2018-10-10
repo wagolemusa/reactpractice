f3afae19f3c09dd9c512f7a913f81616d00206d5
kisumutalent <homiemusa@gmail.com>
2018-10-10 16:39:13 +0300

    create functions listen to DOM events - click mouseover copy and mouse over

--- src/App.js
+++ src/App.js
@@ -6,11 +6,30 @@ class App extends Component{
     name: 'homie',
     age:  '25'
   }
+  
+  // function which anable  to click
+  handleClick(e){
+    console.log(e.target);
+  }
+
+  // function which anable Mouse move over
+  handleMouseOver(e){
+    console.log(e.target, e.pageX)
+  }
+  // function sends you massege when your copying 
+  handleCopy(e){
+    console.log('Try beign original for once!');
+  }
+
   render(){
     return (
       <div className="app-connect">
         <h1>Refuge wise</h1>
         <p>my name is {this.state.name} and I'm {this.state.age} old</p>
+        <button onClick={this.handleClick}>Click Me</button>
+        <button onMouseOver={this.handleMouseOver}>On mouse</button>
+
+        <p onCopy={this.handleCopy}>What do you think, you want to be came</p>
       </div>
     )
   }
