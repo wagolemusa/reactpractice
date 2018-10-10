d3537ad6e49f24038821ed6337ca6ca2134cb471
kisumutalent <homiemusa@gmail.com>
2018-10-10 15:11:20 +0300

    create a simple compont in react

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
+
+class App extends Component{
+  render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app-connect">
+        <h1>Refuge wise</h1>
+        <p>{ Math.random() * 10 }</p>
       </div>
-    );
+    )
   }
 }
 
