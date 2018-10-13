3ab28294201aff86b9f1f3c38a16cb0aa00bcfc5
kisumutalent <homiemusa@gmail.com>
2018-10-13 21:23:19 +0300

    using if else conditions i the project

--- src/Nestapp.js
+++ src/Nestapp.js
@@ -3,18 +3,15 @@ import React from 'react';
 // function
 const  Nestapp = ({ ningas }) => {
 
-        // function for calling in array
-        // const { ningas } = props;
-
         // functin to display each data from array
         const ningasList = ningas.map(show => {
-            return (
+            return  show.age > 25 ? (
                 <div className="nest" key={show.id}>
-                <div>Name: {show.name}</div><br/>
-                <div>Age:  {show.age}</div><br/>
-                <div>Balt: {show.belt}</div><br/>
-            </div>
-            )
+                    <div>Name: {show.name}</div><br/>
+                    <div>Age:  {show.age}</div><br/>
+                    <div>Balt: {show.belt}</div><br/>
+                </div>
+            ): null;    
         })
         return (
             <div className="ninja">
