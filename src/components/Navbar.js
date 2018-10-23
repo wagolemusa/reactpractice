bf8ab565c52e134e9b30db798b9ffbb653a2dc4e
kisumutalent <homiemusa@gmail.com>
2018-10-23 20:58:02 +0300

    using NavLinks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react'
-
+import { Link, NavLink } from 'react-router-dom'
+ 
 const Navbar = () => {
     return (
         <nav className="nav-wrapper red darken-3">
             <div className="container">
                 <a className="brand-logo">Dj Refuge Wise</a>
                 <ul className="right">
-                    <li><a href="/home">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contant</a></li>
+                    <li><Link to="home">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/contact">Contant</Link></li>
                 </ul>
             </div>
         </nav>
